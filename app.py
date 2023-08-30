2f509e94471a3cf735ac4d4b4e24ec20ae40a505
Michael <michaellngriffin@gmail.com>
2023-08-30 15:30:46 -0400

    app skeleton and html basics added, tests next

--- app.py
+++ app.py
@@ -12,3 +12,55 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
+
+
+# GET /
+#Redirect to list of users. (We’ll fix this in a later step).
+@app.get('/')
+def show_home_page():
+    """List of users, button to add user."""
+
+# GET /users
+# Show all users.
+@app.get('/users')
+def show_all_users():
+    """Show all users"""
+#     Have a link here to the add-user form.
+
+# GET /users/new
+#     Show an add form for users
+@app.post('/users/new')
+def show_add_form():
+    """Show an add form for users"""
+#     Make these links to view the detail page for the user.
+
+# POST /users/new
+#     Process the add form, adding a new user and going back to /users
+@app.post('/users/new')
+def add_new_user():
+    """Process the add form, adding a new user and going back to /users"""
+
+# GET /users/[user-id]
+@app.get('/users/<int: user_id>')
+def show_user_details(user_id):
+#     Show information about the given user.
+#     Have a button to get to their edit page, and to delete the user.
+
+
+# GET /users/[user-id]/edit
+@app.get('/users/<int: user_id>/edit')
+def show_edit_details(user_id):
+#     Show the edit page for a user.
+#     Have a cancel button that returns to the detail page for a user, and a save button that updates the user.
+
+
+@app.post('/users/<int: user_id>/edit')
+def add_edit_details(user_id):
+# POST /users/[user-id]/edit
+# Process the edit form, returning the user to the /users page.
+
+
+@app.post('/users/<int: user_id>/delete')
+def delete_user(user_id):
+# POST /users/[user-id]/delete
+# Delete the user.
--- models.py
+++ models.py
@@ -1,1 +1,33 @@
 """Models for Blogly."""
+
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    app.app_context().push()
+    db.app = app
+    db.init_app(app)
+
+class User(db.Model):
+    """User documentation"""
+    __tablename__ = "users"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True)
+
+    first_name = db.Column(
+        db.String(30),
+        nullable=False)
+
+    last_name = db.Column(
+        db.String(30),
+        nullable=False)
+
+    image_url = db.Column(
+        db.String(80),
+        nullable=True,
+        default=None)
